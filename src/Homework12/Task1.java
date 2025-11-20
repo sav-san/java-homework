7aa40a5cfe28a6154e1936f5f9845d2aa88cea83
sav-san <sav_san@icloud.com>
2025-11-21 02:02:59 +0300

    werfq

--- src/Homework12/Task1.java
+++ src/Homework12/Task1.java
@@ -2,6 +2,8 @@ package Homework12;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 class Task1 extends JFrame {
@@ -9,16 +11,17 @@ class Task1 extends JFrame {
     private Random random;
 
 
+
+
     public Task1() {
         setSize(800, 600);
 
         random = new Random();
         shapes = new Shape[20];
-
         for (int i = 0; i < shapes.length; i++) {
             Color color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
             int x = random.nextInt(800);
-            int y = random.nextInt(600);
+            int y = random.nextInt(100,600);
 
             if (random.nextBoolean()) {
                 int radius = 10 + random.nextInt(100);
@@ -29,6 +32,36 @@ class Task1 extends JFrame {
                 shapes[i] = new Rectangle(color, x, y, width, height);
             }
         }
+        JButton rect = new JButton("Прямоугольники");
+        rect.setSize(100,100);
+        rect.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < shapes.length; i++){
+                    if (shapes[i] instanceof Rectangle){
+                        shapes[i]=null;
+                    }
+                }
+                repaint();
+            }
+        });
+
+        JButton circ = new JButton("Круги");
+        circ.setSize(100,100);
+        circ.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < shapes.length; i++){
+                    if (shapes[i] instanceof Circle){
+                        shapes[i]=null;
+                    }
+                }
+                repaint();
+            }
+        });
+        setLayout(new FlowLayout());
+        add(rect);
+        add(circ);
 
         setVisible(true);
     }
@@ -45,5 +78,6 @@ class Task1 extends JFrame {
 
     public static void main(String[] args) {
         new Task1();
+
     }
 }
