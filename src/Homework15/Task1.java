15c92d3e252decc09378d5f0ddf76efeccf33ae5
sav-san <sav_san@icloud.com>
2025-12-01 15:18:52 +0300

    Улучшена 15 практика

--- src/Homework15/Task1.java
+++ src/Homework15/Task1.java
@@ -16,9 +16,9 @@ class LabExample extends JFrame{
         setLocationRelativeTo(null);
         setLayout(new FlowLayout());
         setSize(200,200);
-        add(new JLabel("1st Number"));
+        add(new JLabel("Число 1:"));
         add(jta1);
-        add(new JLabel("2nd Number"));
+        add(new JLabel("Число 2:"));
         add(jta2);
         add(addButton);
         addButton.addActionListener(new ActionListener(){
@@ -26,9 +26,9 @@ class LabExample extends JFrame{
                 try{
                     double x1 = Double.parseDouble(jta1.getText().trim());
                     double x2 = Double.parseDouble(jta2.getText().trim());
-                    JOptionPane.showMessageDialog(null, "Result = "+(x1+x2),"Answer",JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Результат = "+(x1+x2),"Ответ",JOptionPane.INFORMATION_MESSAGE);
                 }catch(Exception e){
-                    JOptionPane.showMessageDialog( null, "Error in Numbers!","ALERT!" , JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog( null, "Ошибка в числах","ALERT!" , JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
@@ -38,9 +38,9 @@ class LabExample extends JFrame{
                 try{
                     double x1 = Double.parseDouble(jta1.getText().trim());
                     double x2 = Double.parseDouble(jta2.getText().trim());
-                    JOptionPane.showMessageDialog(null, "Result = "+(x1-x2),"Answer",JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Результат = "+(x1-x2),"Ответ",JOptionPane.INFORMATION_MESSAGE);
                 }catch(Exception e){
-                    JOptionPane.showMessageDialog( null, "Error in Numbers!","ALERT!" , JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog( null, "Ошибка в числах","ALERT!" , JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
@@ -50,9 +50,9 @@ class LabExample extends JFrame{
                 try{
                     double x1 = Double.parseDouble(jta1.getText().trim());
                     double x2 = Double.parseDouble(jta2.getText().trim());
-                    JOptionPane.showMessageDialog(null, "Result = "+(x1*x2),"Answer",JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Результат = "+(x1*x2),"Ответ",JOptionPane.INFORMATION_MESSAGE);
                 }catch(Exception e){
-                    JOptionPane.showMessageDialog( null, "Error in Numbers!","ALERT!" , JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog( null, "Ошибка в числах","ALERT!" , JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
@@ -62,9 +62,9 @@ class LabExample extends JFrame{
                 try{
                     double x1 = Double.parseDouble(jta1.getText().trim());
                     double x2 = Double.parseDouble(jta2.getText().trim());
-                    JOptionPane.showMessageDialog(null, "Result = "+(x1/x2),"Answer",JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Результат = "+(x1/x2),"Ответ",JOptionPane.INFORMATION_MESSAGE);
                 }catch(Exception e){
-                    JOptionPane.showMessageDialog( null, "Error in Numbers!","ALERT!" , JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog( null, "Ошибка в числах","ALERT!" , JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
